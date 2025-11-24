31a22573ff46753e4c2914d3af4582128816e000
innovaft.co <innovaft.co@gmail.com>
2025-11-24 19:15:41 +0530

    input height

--- src/design-system/atoms/input/Input.tsx
+++ src/design-system/atoms/input/Input.tsx
@@ -11,7 +11,7 @@ const Input: React.FC<InputProps> = ({
 }) => {
   const variants: Record<string, string> = {
     primary:
-      "border outline-none w-full border-primary rounded-xl p-3 placeholder:text-primary text-black tracking-032 leading-l1 text-base ",
+      "border outline-none w-full border-primary rounded-xl h-12 max-h-12 min-h-12 px-3 py-2 placeholder:text-primary text-black tracking-032 leading-l1 text-base ",
     underline:
       "border-b-2 border-gray-400 focus:border-primary outline-none bg-transparent",
     minimal: "bg-transparent border-none outline-none px-2 py-1",
